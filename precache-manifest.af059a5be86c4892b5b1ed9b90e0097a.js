7b26d8312b50cbdb6511f5abdb0b0beb4c2d3a45
mateusfg7 <mateusfg77@gmail.com>
2019-12-14 08:54:44 -0300

    Updates

---
+++ precache-manifest.af059a5be86c4892b5b1ed9b90e0097a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdde96da35449dd558cab5dc065e165",
+    "url": "/hora_atual/index.html"
+  },
+  {
+    "revision": "b8d883c0318ff58b9112",
+    "url": "/hora_atual/static/css/main.8c5dcf27.chunk.css"
+  },
+  {
+    "revision": "9e69f34950653ad70366",
+    "url": "/hora_atual/static/js/2.ba7edda5.chunk.js"
+  },
+  {
+    "revision": "b8d883c0318ff58b9112",
+    "url": "/hora_atual/static/js/main.9191dd6f.chunk.js"
+  },
+  {
+    "revision": "3e4daa37a5e80f0d46b8",
+    "url": "/hora_atual/static/js/runtime-main.6b6cf792.js"
+  }
+]);
